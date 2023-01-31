5b4ce9e580bfe29b2d5b927246afdd6378517c26
raissa <kouabenanraissa@yahoo.fr>
2023-01-31 09:19:03 +0000

    first commit

---
+++ exercise.php
@@ -0,0 +1,20 @@
+<?php
+#Exercise1
+$mot = "Bienvenue";
+echo strlen($mot);
+
+?>
+
+
+<?php
+#Exercise 2
+  $strng ="Bienvenue  ADACI";
+  echo str_word_count($strng); 
+?>
+
+<?php
+
+#Exercise3
+    $stng = "Bonjour Abidjan";
+    echo strtoupper($stng)
+?>
